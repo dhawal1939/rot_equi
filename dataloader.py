81ee417d6ea5cfafce6517c46a6819bbd833dd82
thesigmaguy <dhawal.sirikoda@research.iiit.ac.in>
2022-06-17 22:57:06 +0530

    test

--- dataloader.py
+++ dataloader.py
@@ -7,6 +7,7 @@ import math
 import torch
 import imageio
 import numpy as np
+from torch import nn
 from config import *
 from torch.utils.data import Dataset
 
@@ -46,42 +47,47 @@ class DirectionColorHDRDataset(Dataset):
 
     def resize(self, resolution):
         self.resolution = resolution
-        self.env_maps = [cv2.imread(env_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH) for env_path in self.file_list]
+        self.env_maps = [cv2.resize(cv2.imread(env_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH), 
+                                    (2 * self.resolution, self.resolution)) for env_path in self.file_list]
+        self.env_maps = torch.from_numpy(np.array(self.env_maps))
+        # self.env_maps = np.asarray([cv2.imread(env_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH) for env_path in self.file_list])
+        # self.env_maps = torch.from_numpy(self.env_maps).permute(0, 3, 1, 2)
+        # print(self.env_maps.min(), self.env_maps.max(), self.env_maps.shape)
+        # self.env_maps = nn.functional.interpolate(self.env_maps, mode='bilinear', size=[self.resolution, 2 * self.resolution])
+        # print(self.env_maps.min(), self.env_maps.max(), self.env_maps.shape, self.env_maps.dtype)
+        torch.clamp_(self.env_maps, min = 1e-20, max = 1e30)
 
-        self.env_maps = [np.array(cv2.resize(env_map, (2 * self.resolution, self.resolution)))
-                         for env_map in self.env_maps]
-        self.env_maps = torch.from_numpy(np.array(self.env_maps).reshape(-1, 1))
+        # min_val = torch.min(self.env_maps)
+        # max_val = torch.max(self.env_maps)
+        # if min_val <= 0.:
+        #     self.env_maps = self.env_maps - min_val + 1e-15  # add epsilon
 
-        min_val = torch.min(self.env_maps)
-        max_val = torch.max(self.env_maps)
-        if min_val <= 0.:
-            self.env_maps = self.env_maps - min_val + 1e-5  # add epsilon
-        if max_val == torch.inf:
-            self.env_maps[self.env_maps == torch.inf] = 1e30
+        # if max_val == torch.inf:
+        #     self.env_maps[self.env_maps == torch.inf] = 1e30
 
-        self.env_maps = self.env_maps.view(-1, 2 * self.resolution * self.resolution, 3)
+
+        self.env_maps = self.env_maps.reshape(-1, 2 * self.resolution * self.resolution, 3)
 
         # log and scale
         self.env_maps = torch.log(self.env_maps)
 
-        # for i, e in enumerate(self.env_maps):
-        #     if np.isnan(e).any():
-        #         print(e.min(), e.max(), np.isnan(e).sum(), flush=True)
-        #         raise Exception(f"Error nan in {i}")
-        #     if np.isinf(e).any():
-        #         print(e.min(), e.max(), np.isnan(e).sum(), flush=True)
-        #         # raise Exception(f"Error inf in {i}")
-        # exit(-1)
 
         for i in range(3):  # RGB
             _max, _min = torch.max(self.env_maps[:, :, i]), torch.min(self.env_maps[:, :, i])
             self.env_maps[:, :, i] = (self.env_maps[:, :, i] - _min) / (_max - _min)
         self.env_maps[:, :, :] = 2 * self.env_maps - 1
 
-        # self.env_maps = [imageio.imread(env_path, format='HDR-FI') for env_path in self.file_list]
-        # self.env_maps = [torch.from_numpy(np.array(img.resize(self.resolution, 
-        #                                            2 * self.resolution)).reshape(-1, 3)) 
-        #                                            for img in self.env_maps]
+        # print(self.env_maps.shape, resolution)
+        # if self.resolution > 32:
+        #     e = np.array(self.env_maps)
+        #     if np.isnan(e).any():
+        #         print(e.min(), e.max(), np.isnan(e).sum(), flush=True)
+        #         # raise Exception(f"Error nan in {i}")
+        #     if np.isinf(e).any():
+        #         print(e.min(), e.max(), np.isnan(e).sum(), flush=True)
+        #         raise Exception(f"Error inf in {i}")
+        #     exit(-1)
+
         self.directions = self.get_directions()
 
     def __len__(self):
--- train.py
+++ train.py
@@ -21,7 +21,7 @@ _log_dir.absolute().mkdir(exist_ok=True, parents=True)
 _save_path = Path(log_dir_path) / datetime.now().strftime("%Y_%m_%d_%H_%M")
 Path(_save_path).absolute().mkdir(exist_ok=True, parents=True)
 
-train_files_list = sorted(glob.glob(str(train_file_dir) + '/*.exr'))
+train_files_list = sorted(glob.glob(str(train_file_dir) + '/*.exr'))[:500]
 test_files_list = sorted(glob.glob(str(test_file_dir) + '/*.exr'))
 
 writer = SummaryWriter(_log_dir)
@@ -29,14 +29,16 @@ writer = SummaryWriter(_log_dir)
 N_val = latent_dim // 3
 model = EquiVarianceIllumination(in_dim=N_val, inter_dim=N_val ** 2 + N_val, num_envs=len(train_files_list))
 
-train_dataset = DirectionColorHDRDataset(train_files_list,
-                                         resolution=initial_resolution)
-train_loader = DataLoader(train_dataset,
-                          shuffle=False,
-                          batch_size=1,
-                          num_workers=0)
 
 current_resolution = initial_resolution
+
+train_dataset = DirectionColorHDRDataset(train_files_list,
+                                    resolution=current_resolution)
+train_loader = DataLoader(train_dataset,
+                        shuffle=True,
+                        batch_size=1,
+                        num_workers=0)
+
 
 optim = torch.optim.Adam(params=model.parameters(),
                          lr=learning_rate)
@@ -51,14 +53,16 @@ if load_wt:
     model.load_state_dict(check_point['model_state_dict'])
     start_epoch = check_point['epoch']
     optim.load_state_dict(check_point['optimizer_state_dict'])
-    # current_resolution = check_point['current_resolution']
+    current_resolution = check_point['current_resolution']
 
 model.train()
 for epoch in range(start_epoch, epochs):
+
     if epoch in [800, 1600]:
         current_resolution *= 2
         train_dataset.resize(current_resolution)
 
+    print(f'Epoch: {epoch}', flush=True)
     # loss accumulation
     recons_loss = torch.Tensor([0]).to(device)
     kld_loss = torch.Tensor([0]).to(device)
@@ -69,6 +73,10 @@ for epoch in range(start_epoch, epochs):
     optim.zero_grad()
     for i, batch in enumerate(train_loader):
         directions, gt_rgb_vals, sin_theta, env_idx = batch
+        
+        # print('pkb', directions.shape, gt_rgb_vals.shape, sin_theta.shape, 
+        # torch.sum(torch.isnan(gt_rgb_vals)), torch.sum(torch.isnan(directions)))
+        
         directions = directions.to(device)
         gt_rgb_vals = gt_rgb_vals.to(device)
         sin_theta = sin_theta.to(device)
@@ -76,21 +84,24 @@ for epoch in range(start_epoch, epochs):
         pred_vals, mu, var = model(directions, env_idx)
 
         recons_loss += (1 / sin_theta.shape[1]) * torch.sum(sin_theta.t() * (pred_vals - gt_rgb_vals[0]) ** 2)
-        kld_loss += torch.sum(1 + torch.log(var) - var - mu)
+        kld_loss += torch.sum(1 + torch.log(var) - var - mu ** 2)
+        torch.cuda.empty_cache()
         pbar.update()
     pbar.close()
 
     loss += recons_loss
-    loss += (beta_kld / latent_dim) * (0.5 * kld_loss)
-    print(f'Loss: {loss.item()}, Epoch: {epoch}', flush=True)
+    loss -= (beta_kld / latent_dim) * (0.5 * kld_loss)
+
+    loss.backward()
+    optim.step()
+    scheduler.step()
+
+    print(f'Loss: {loss.item()}', flush=True)
     writer.add_scalar('Loss/train', loss.item(), epoch)
     writer.add_image('pred_env_map', torch.e ** pred_vals.view(current_resolution, 2 * current_resolution, 3),
                      global_step=epoch, dataformats='HWC')   # probably reverse channel
     writer.add_image('gt_env_map', torch.e ** gt_rgb_vals[0].view(current_resolution, 2 * current_resolution, 3),
                      global_step=epoch, dataformats='HWC')  # probably reverse channel
-    loss.backward()
-    optim.step()
-    scheduler.step()
 
     if epoch % 100 == 0:
         torch.save({
